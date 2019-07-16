7b8b3175f72e1c0785fb31b8f82f894da21d516d
Evette Telyas <etelyas@gmail.com>
2019-07-16 07:53:53 -0600

    edit game extraction from csv to set game_id as hash key
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,8 @@ class Game
 
   def initialize(row)
     @game_id = row[:game_id]
-    @away_goals = row[:away_goals]
-    @home_goals = row[:home_goals]
+    @away_goals = row[:away_goals].to_i
+    @home_goals = row[:home_goals].to_i
   end
 
 
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -9,14 +9,14 @@ class StatTracker
   end
 
   def self.from_csv
-    @games = create_games('./data/game.csv')
+    self.create_games
   end
 
   def self.create_games
     @games = {}
     options = {:headers => true, :header_converters => :symbol}
     CSV.foreach('./cross_check/data/game.csv', options) do |row|
-      @games[row.first.each] = Game.new(row)
+      @games[row.first[1]] = Game.new(row)
     end
   end
 
