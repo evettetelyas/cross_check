95e345d8d939cea6c5b28a57ffeae6a96d5120c6
Evette Telyas <etelyas@gmail.com>
2019-07-16 11:39:05 -0600

    finalize game data to objects
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,28 +11,17 @@ class StatTracker
     @games = games
   end
 
-  def self.from_csv
-    self.create_games
-  end
-
-  def self.dummy_from_csv
-    self.dummy_create_games
-  end
-
-  def self.create_games
+  def self.create_games(location)
     @games = {}
     options = {:headers => true, :header_converters => :symbol}
-    CSV.foreach('./cross_check/data/game.csv', options) do |row|
+    CSV.foreach(location, options) do |row|
       @games[row.first[1]] = Game.new(row)
     end
   end
 
-  def self.dummy_create_games
-    @games = {}
-    options = {:headers => true, :header_converters => :symbol}
-    CSV.foreach('./cross_check/dummy_data/dummy_game.csv', options) do |row|
-      @games[row.first[1]] = Game.new(row)
-    end
+  def self.from_csv(locations)
+    games = create_games(locations[:games])
+    StatTracker.new(@games)
   end
-binding pry
+
 end
--- runner.rb
+++ runner.rb
@@ -3,11 +3,18 @@ require './lib/stat_tracker'
 game_path = './data/game.csv'
 team_path = './data/team_info.csv'
 game_teams_path = './data/game_teams_stats.csv'
+dummy_game_path = './dummy_data/dummy_game.csv'
+dummy_team_path = './dummy_data/dummy_team_info.csv'
+dummy_game_teams_path = './dummy_data/dummy_game_teams_stats.csv'
 
 locations = {
   games: game_path,
   teams: team_path,
-  game_teams: game_teams_path
+  game_teams: game_teams_path,
+  dummy_games: dummy_game_path,
+  dummy_teams: dummy_team_path,
+  dummy_game_teams: dummy_game_teams_path
 }
 
 stat_tracker = StatTracker.from_csv(locations)
+binding pry
