97a43d8c5db43eaba2ef87ee48ca70d4ee20fdcd
Evette Telyas <etelyas@gmail.com>
2019-07-16 11:39:57 -0600

    add lowest_total_score
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- modules/game_statables.rb
+++ modules/game_statables.rb
@@ -5,4 +5,9 @@ module GameStatables
     return all_goals.max
   end
 
+  def lowest_total_score
+    all_goals = @games.map {|key,value| value.away_goals + value.home_goals}
+    return all_goals.min
+  end
+
 end
